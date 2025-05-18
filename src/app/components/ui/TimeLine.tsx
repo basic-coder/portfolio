610b91229388b76ea813f9a2c4f46ed8d656b9a0
sakpalbhavesh <bhavesh.sakpal@deptagency.com>
2025-05-19 01:29:54 +0530

    font size changed

--- src/app/components/ui/TimeLine.tsx
+++ src/app/components/ui/TimeLine.tsx
@@ -37,7 +37,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             ref={containerRef}
         >
             <div className="max-w-7xl mx-auto pt-20 px-4 md:px-8 lg:px-10">
-                <h2 className="text-lg md:text-4xl font-bold mb-4 text-black dark:text-white max-w-4xl">
+                <h2 className="text-2xl md:text-4xl font-bold mb-4 text-black dark:text-white max-w-4xl">
                     Journey as a Frontend Developer
                 </h2>
                 <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-lg lg-text-2xl max-w-4xl">
