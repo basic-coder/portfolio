c27eb06d372468a862c26825d99687e3b4a64c0c
basic-coder <sakpalbhavesh2001@gmail.com>
2025-05-19 22:38:56 +0530

    dark theme fixes

--- src/app/components/ui/GridBackground.tsx
+++ src/app/components/ui/GridBackground.tsx
@@ -21,8 +21,7 @@ export function GridBackground(props: Props) {
                 className={cn(
                     "absolute inset-0 ",
                     "[background-size:50px_50px]",
-                    "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-                    "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+                    "[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
                 )}
             />
             {/* Radial gradient for the container to give a faded look */}
